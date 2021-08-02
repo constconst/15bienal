f5636ca57357162d6fccaa4f5c3da851f18542ee
Constanza Barrios <co.fuenzalida@gmail.com>
2021-08-02 12:08:38 -0400

    interacciones del header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 $(function() {
-    console.log("ready!");
+    // Carousel
     $('.carousel.carousel-slider').carousel({
         fullWidth: true,
         indicators: true
+    });
+    // Hidden nav
+    var prev = 0;
+    var $window = $(window);
+    var hiddennav = $('.header__container-hidden');
+
+    $window.on('scroll', function() {
+        var scrollTop = $window.scrollTop();
+        hiddennav.toggleClass('hidden', scrollTop > prev);
+        prev = scrollTop;
+    });
+
+    // open mobile menu
+    $('.js-toggle-menu').click(function(e) {
+        e.preventDefault();
+        $('.mobile-header-nav').slideToggle();
+        $(this).toggleClass('open');
     });
 });
