8477fb26a559dcf933eea5bc5c66a5dd2aad3cc7
Constanza Barrios <co.fuenzalida@gmail.com>
2021-09-07 11:16:32 -0300

    test de slider

--- js/script.js
+++ js/script.js
@@ -27,3 +27,68 @@ $(function() {
 
 
 });
+
+// Glider init
+
+window.addEventListener('load', function() {
+    document.querySelector('.glider').addEventListener('glider-slide-visible', function(event) {
+        var glider = Glider(this);
+        console.log('Slide Visible %s', event.detail.slide)
+    });
+    document.querySelector('.glider').addEventListener('glider-slide-hidden', function(event) {
+        console.log('Slide Hidden %s', event.detail.slide)
+    });
+    document.querySelector('.glider').addEventListener('glider-refresh', function(event) {
+        console.log('Refresh')
+    });
+    document.querySelector('.glider').addEventListener('glider-loaded', function(event) {
+        console.log('Loaded')
+    });
+
+    window._ = new Glider(document.querySelector('.glider'), {
+        slidesToShow: 1, //'auto',
+        slidesToScroll: 1,
+        draggable: true,
+        scrollLock: false,
+
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive: [{
+                breakpoint: 800,
+                settings: {
+                    slidesToScroll: 3,
+                    slidesToShow: 3,
+                    exactWidth: false
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToScroll: 3,
+                    slidesToShow: 3,
+                    dots: false,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToScroll: 2,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToScroll: 2,
+                    slidesToShow: 2,
+                    dots: false,
+                    arrows: false,
+                    scrollLock: true
+                }
+            }
+        ]
+    });
+});
